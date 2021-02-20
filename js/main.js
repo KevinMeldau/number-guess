bc1eb39570c47329ca9c9b891ef284be48077558
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-02-20 18:36:38 -0500

    Remove unwanted commented code

--- js/main.js
+++ js/main.js
@@ -42,15 +42,6 @@ function checkGuess() {
 
 guessSubmit.addEventListener("click", checkGuess);
 
-// function setGameOver() {
-//   guessField.disabled = true;
-//   guessSubmit.disabled = true;
-//   resetButton = document.createElement("button");
-//   resetButton.textContent = "Start new game";
-//   document.body.appendChild(resetButton);
-//   resetButton.addEventListener("click", resetGame);
-// }
-
 function resetGame() {
   guessCount = 1;
   const resetParas = document.querySelectorAll(".resultParas p");
